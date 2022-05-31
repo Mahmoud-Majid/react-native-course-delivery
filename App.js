bdfdcb892ad246e22a720912d1c7437db19d2b75
Mahmoud Majid <76546155+Mahmoud-Majid@users.noreply.github.com>
2022-05-31 20:44:41 +0200

    first commit

--- App.js
+++ App.js
@@ -1,111 +1,98 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable react/jsx-no-undef */
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+import {SafeAreaView, StyleSheet, View, Text, FlatList, TextInput} from 'react-native';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  const [data, setData] = useState([]);
+  
+  const [filteredData, setfilteredData] = useState([]);
+  const [search, setSearch] = useState('');
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  useEffect(() => {
+    getPostsData();
+  }, []);
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const getPostsData = async () => {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/posts',
+    );
+    if (response.status === 200) {
+      setData(response.data);
+      setfilteredData(response.data);
+    }
+    if (response.status === 404) {
+      console.log('Not Found');
+    }
   };
 
+  const ItemView = ({item}) => {
+    return (
+      <Text style={styles.itemStyle}>
+        {item.id}
+        {'. '}
+        {item.title.toUpperCase()}
+      </Text>
+    );
+  };
+
+  const searchFilter = text => {
+    if(text) {
+      const newData = data.filter(item => {
+      return item.title.toLowerCase().includes(text.toLowerCase());
+      });   
+      setfilteredData(newData);
+      setSearch(text);
+    }else {
+      setfilteredData(data);
+      setSearch(text);
+    }
+  }
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView >
+      <View>
+        <TextInput 
+        style={styles.textInputStyle} 
+        placeholder="Search"
+        underlineColorAndroid={'transparent'}
+        value={search}
+        onChangeText={searchFilter}
+        />
+        <FlatList
+          data={filteredData }
+          keyExtractor={item => item.id.toString()}
+          ItemSeparatorComponent={() => (
+            <View
+              style={{
+                height: 1,
+                backgroundColor: '#c8c8c8',
+              }}
+            />
+          )}
+          renderItem={ItemView}
+        />
+      </View>
     </SafeAreaView>
   );
 };
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    backgroundColor: '#fff',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  itemStyle: {
+    padding: 15,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  textInputStyle: {
+    height: 50,
+    borderWidth: 1,
+    paddingLeft: 20,
+    margin: 5,
+    borderColor: '#009688',
+    backgroundColor: '#fff',
   },
 });
 
